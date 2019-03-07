2a9e81172f847d6cc3247cfb897ac9b208e05af6
Brandon <mbrandon@labs.neit.edu>
2019-03-07 13:52:52 -0500

    finished project

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var io = require('socket.io')(process.env.PORT||5000);
 var shortid = require('shortid');
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var mongoose = require('mongoose');
+
+var db = require('./config/database');
+
+//gets rid of warning 
+mongoose.Promise = global.Promise;
+
+//connect to mongodb using mongoose
+mongoose.connect(db.mongoURI, {
+    useMongoClient:true
+}).then(function(){console.log("MongoDB connected")})
+.catch(function(err){console.log(err)});
+
+//Load in Models
+require('./models/Users');
+var Users = mongoose.model('Users');
 
 var players = [];
+
 var playerCount = 0;
 
 console.log("SERVER LOG: Server Running");
 
 io.on('connection', function(socket){
     console.log("SERVER LOG: Connected to Unity");
+    socket.emit('connected');
     var thisPlayerId = shortid.generate();
 
     var player = {
@@ -30,13 +52,32 @@ io.on('connection', function(socket){
         console.log("SERVER LOG: Sending spawn to new with ID ",thisPlayerId);
     }
 
+    socket.on('senddata', function(data){
+        console.log(JSON.stringify(data));
+    var newUser = {
+        name:data.name,
+    }
+    new Users(newUser)
+    .save()
+    .then(function(users){
+        console.log("sending data to database");
+        Users.find({})
+        .then(function(users){
+            console.log(users);
+            socket.emit('hideform', {users});
+        });
+
+        
+    });
+});
+
     socket.on('sayhello', function(data){
         console.log("SERVER LOG: Unity Game says hello");
         socket.emit('talkback');
     });
 
     socket.on('disconnect', function(){
-        console.log("SERVER LOG: player disconnected");
+        console.log("SERVER LOG: Player ", {id:thisPlayerId}, " disconnected");
         delete players[thisPlayerId];
         socket.broadcast.emit('disconnected', {id:thisPlayerId})
     });
